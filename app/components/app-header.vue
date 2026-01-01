dc61b41f92e1f264af35ac4d486100184adaf537
mg89.shin <asanswl@naver.com>
2026-01-01 21:59:19 +0900

    1

---
+++ app/components/app-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold"> Finance Tracker </NuxtLink>
+    <div>
+      <UAvatar src="https://github.com/benjamincanac.png" />
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+  
+</script>
+
+<style></style>
---
+++ app/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu :items="transactionViewOptions" v-model="selectedView" />
+    </div>
+  </section>
+  <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
+    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false"/>
+    <Trend color="red" title="Income" :amount="4000" :last-amount="3000" :loading="false"/>
+    <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false"/>
+    <Trend color="red" title="Income" :amount="4000" :last-amount="3000" :loading="false"/>
+
+  </section>
+</template>
+
+<script lang="ts" setup>
+const selectedView = ref(transactionViewOptions[1]);
+</script>
+
+<style></style>
